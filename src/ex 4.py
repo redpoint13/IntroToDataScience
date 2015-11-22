15308af9717884f50fc76eaf5f73998a81b17628
Tony <redpoint13@gmail.com>
2015-11-22 13:57:42 -0700

    initial commit of Udacity Intro to Data Science files

---
+++ src/ex 4.py
@@ -0,0 +1,41 @@
+import numpy
+from pandas import DataFrame, Series
+
+
+def avg_medal_count():
+    '''
+    Using the dataframe's apply method, create a new Series called 
+    avg_medal_count that indicates the average number of gold, silver,
+    and bronze medals earned amongst countries who earned at 
+    least one medal of any kind at the 2014 Sochi olympics.  Note that
+    the countries list already only includes countries that have earned
+    at least one medal. No additional filtering is necessary.
+    
+    You do not need to call the function in your code when running it in the
+    browser - the grader will do that automatically when you submit or test it.
+    '''
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea', 
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+    
+    olympic_medal_counts = {'country_name':countries,
+                            'gold': Series(gold),
+                            'silver': Series(silver),
+                            'bronze': Series(bronze)}    
+    df = DataFrame(olympic_medal_counts)
+    
+    # YOUR CODE HERE
+    avg_country_medal_count = numpy.mean(df, axis=1)  # to take the mean: 1->row 0->column
+    df['avg_medal_count'] = avg_medal_count           # adds the average medal count for each country to df
+    avg_medal_count = numpy.mean(df, axis=0)          # returns the average of gold, silver and bronze medals
+    
+    
+    return avg_medal_count
